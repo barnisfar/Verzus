c8f17aa6bb2b98533fb44bace82a7b48c6b2b771
Daniel Nielsen <dn@speakhuman.dk>
2012-09-19 14:36:57 +0200

    commit 4

---
+++ Models/VerzusStructure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Verzus.Models
+{
+    public class VerzusStructure
+    {
+        public Verzus vs { get; set;}
+        public VerzusItem vsi1 { get; set; }
+        public VerzusItem vsi2 { get; set; }
+        public VerzusItem vsiC { get; set; }
+    }
+}
